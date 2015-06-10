bd15803f10732c7e6b0ebaf8639a4270d2d47ccb
unknown <kenwong@KenDevPc.ntdev.corp.microsoft.com>
2015-06-09 18:53:50 -0700

    Adding new sample for Windows.Media.Import

---
+++ MediaImport/cs/DataBinding/GeneratorIncrementalLoadingClass.cs
@@ -0,0 +1,54 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    // This class implements IncrementalLoadingBase. 
+    // To create your own Infinite List, you can create a class like this one that doesn't have 'generator' or 'maxcount', 
+    //  and instead downloads items from a live data source in LoadMoreItemsOverrideAsync.
+    public class GeneratorIncrementalLoadingClass<T> : IncrementalLoadingBase
+    {
+        public GeneratorIncrementalLoadingClass(uint maxCount, Func<int, T> generator)
+        {
+            _generator = generator;
+            _maxCount = maxCount;
+        }
+
+       protected async override Task<IList<object>> LoadMoreItemsOverrideAsync(System.Threading.CancellationToken c, uint count)
+        {
+            uint toGenerate = System.Math.Min(count, _maxCount - _count);
+
+            // This code simply generates
+            var values = from j in Enumerable.Range((int)_count, (int)toGenerate)
+                         select (object)_generator(j);
+            _count += toGenerate;
+
+            return await Task.FromResult<IList<object>>(values.ToArray());
+        }
+
+        protected override bool HasMoreItemsOverride()
+        {
+            return _count < _maxCount;
+        }
+
+        #region State
+
+        Func<int, T> _generator;
+        uint _count = 0;
+        uint _maxCount;
+
+        #endregion 
+    }
+}
