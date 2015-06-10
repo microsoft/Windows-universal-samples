81213b54007c94e2eb8b1bc5b5ffbf0db864e218
Diego Perez Botero <dibotero@microsoft.com>
2015-06-10 09:54:11 -0700

    Another minor fix to a copyright banner

--- datagramsocket/js/js/scenario1_Start.js
+++ datagramsocket/js/js/scenario1_Start.js
@@ -5,9 +5,9 @@
 // THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
 // ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
 // IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-    // PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-    //
-    //*********************************************************
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
 
 (function () {
     "use strict";
