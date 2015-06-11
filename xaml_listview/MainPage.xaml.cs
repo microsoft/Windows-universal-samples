929d2530098a973e2a4097fb0a8f53bc1749ebad
Mike Henderlight <mhender@microsoft.com>
2015-06-11 13:19:23 -0700

    Initial publish

---
+++ xaml_listview/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ListViewSample
+{
+    /// <summary>
+    /// This is the landing page used to navigate to all different scenarios.  See Solution Explorer
+    /// for the code of disctinct scenarios.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void ShowSliptView(object sender, RoutedEventArgs e)
+        {
+            MySamplesPane.SamplesSplitView.IsPaneOpen = !MySamplesPane.SamplesSplitView.IsPaneOpen;
+        }
+    }
+}
